1d5800ef641bd728a43e03a7d1cd5c76db57364f
Ruslan-Suvorov <bloodsrv@gmail.com>
2023-11-01 16:42:01 +0200

    create multi form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { useForm, useFieldArray, useWatch, Control } from "react-hook-form";
+import Header from "./Header";
+import "./App.css";
 
-function App() {
+type FormValues = {
+  listName: string;
+  cart: {
+    name: string;
+    price: number;
+    quantity: number;
+  }[];
+};
+
+let renderCount = 0;
+
+const Total = ({ control }: { control: Control<FormValues> }) => {
+  const formValues = useWatch({
+    name: "cart",
+    control
+  });
+  const total = formValues.reduce(
+    (acc, current) => acc + (current.price) * (current.quantity),
+    0
+  );
+  return <p>Загалом: {total}</p>;
+};
+
+export default function App() {
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormValues>({
+    defaultValues: {
+      cart: [{ name: "Продукт", quantity: 1, price: 10 }]
+    },
+    mode: "onBlur"
+  });
+  const { fields, append, remove } = useFieldArray({
+    name: "cart",
+    control
+  });
+  const onSubmit = (data: FormValues) => console.log(data);
+  renderCount++;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Header
+        renderCount={renderCount}
+      />
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("listName")} placeholder="Product list" />
+        {fields.map((field, index) => {
+          return (
+            <div key={field.id}>
+              <section className={"section"} key={field.id}>
+                <label>Назва
+                <input
+                  placeholder="name"
+                  {...register(`cart.${index}.name` as const, {
+                    required: true
+                  })}
+                  className={errors?.cart?.[index]?.name ? "error" : ""}
+                  defaultValue={field.name}
+                /></label>
+                <label>Кількість
+                <input
+                  placeholder="quantity"
+                  type="number"
+                  {...register(`cart.${index}.quantity` as const, {
+                    valueAsNumber: true,
+                    required: true
+                  })}
+                  className={errors?.cart?.[index]?.quantity ? "error" : ""}
+                  defaultValue={field.quantity}
+                /></label>
+                <label>Вартість
+                <input
+                  placeholder="value"
+                  type="number"
+                  {...register(`cart.${index}.price` as const, {
+                    valueAsNumber: true,
+                    required: true
+                  })}
+                  className={errors?.cart?.[index]?.price ? "error" : ""}
+                  defaultValue={field.price}
+                /></label>
+                <button type="button" onClick={() => remove(index)}>
+                  Видалити
+                </button>
+              </section>
+            </div>
+          );
+        })}
+
+        <Total control={control} />
+
+        <button
+          type="button"
+          onClick={() =>
+            append({
+              name: "",
+              quantity: 1,
+              price: 1
+            })
+          }
         >
-          Learn React
-        </a>
-      </header>
+          Додати продукт
+        </button>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
-
-export default App;
